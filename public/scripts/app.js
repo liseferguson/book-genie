3257b5c634c177eab4f5f98dec775e37c0e5e951
Lise Ferguson <lise.ferguson503@gmail.com>
2018-11-08 20:55:55 -0800

    positioning book image on user info cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -170,14 +170,12 @@ function renderAllLibraries(users){
 			userLibrary = '<li class="empty-library">This library is empty :(</li>'
 		}
 		return  `
+		<img src="https://image.flaticon.com/icons/svg/29/29302.svg" class="book-stack-icon">
 		<div class="user-library-card">
-			<img src="https://image.flaticon.com/icons/svg/29/29302.svg" class="book-stack-icon">
-			<div class="user-info-minicard"
-				<h2 class="firstName">${user.firstName}</h2>
-				<h3 class="city">${user.city}</h3>
-				<h3 class="zipcode">${user.zipcode}</h3>
-				<a href="mailto:${user.email}?Subject=Book%20trade%20request%20from%20your%20neighbor%20on%20Book%20Genie" target="_top">Email ${user.firstName}</a>
-			</div>
+			<h2 class="firstName">${user.firstName}</h2>
+			<h3 class="city">${user.city}</h3>
+			<h3 class="zipcode">${user.zipcode}</h3>
+			<a href="mailto:${user.email}?Subject=Book%20trade%20request%20from%20your%20neighbor%20on%20Book%20Genie" target="_top">Email ${user.firstName}</a>
 			<h3 class="userLibraryTitle"><span>${user.firstName}'s library</span></h3>
 				<ul class="userLibrary">${userLibrary}</ul>
 		</div>   `
